7264f787a7e48ccea6fe84d24d828f76bcde18b0
xinnexx <1467315250@qq.com>
2025-10-18 10:29:51 +0800

    Add files via upload

---
+++ 数组的基础运用.cpp
@@ -0,0 +1,16 @@
+ #include<iostream>
+
+int main()
+{   
+    int arr[10] = { 68868,131211,132,1313,13124,233554,63534,73534,8453,93534534}; 
+    printf("%d\n",arr[7]);
+    int i = 0;
+    while (i<10)
+	{
+		printf("%d\n",arr[i]);
+		i=i+1;
+	}
+	
+	
+	return 0;
+ } 
