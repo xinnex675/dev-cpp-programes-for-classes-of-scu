0ecb5431bbb4b9d7a3fb204d49d1ff8d38e0bf4c
xinnexx <1467315250@qq.com>
2025-10-21 21:47:07 +0800

    Add files via upload

---
+++ 学习检验性质的学习评价系统，目前的所学内容的实践，写完耗费十箱六个核桃.cpp
@@ -0,0 +1,115 @@
+#include<iostream>
+#include<cstdio> 
+#include<conio.h>
+float test(float a ,float b,float c,float d,float e)
+{   float sum = 0;
+    sum = a+b+c+d+e;
+    return sum;
+}
+int main()
+{   
+    char name[20];
+    printf("请输入你的名字：") ;
+    scanf("%s",name);  
+	printf("%s同学您好\n",name);
+	float num1,num2,num3,num4,num5 =0;
+	printf("请以此输入你的语文，数学，英语，物理，赋分科目成绩（用空格隔开）：");
+	scanf("%f %f %f %f %f",&num1,&num2,&num3,&num4,&num5);
+	float sum = test(num1,num2,num3,num4,num5);
+	printf("你的总分是：%.1f\n",sum);
+	if (sum<710 && sum>690){
+		printf("非常优秀，清北水平");
+	   
+	}
+	else if (sum<690&&sum>665){
+		printf("优秀，c9恭候您的到来");
+	   
+	}
+	else if (sum<665&&sum>620){
+		printf("不错，985水平，过年亲友的谈资");
+	    
+	}
+	else if (sum<620&&sum>590){
+		printf("良好，211是稳了");
+	   
+	}
+	else if (sum<590&&sum>540){
+		printf("可以了，一本是多少人的梦想（鼓掌）");
+	    
+	}
+	else if (sum<540&&sum>500){
+		printf("好好自学，争取考研");
+	   
+	}
+	else if (sum<500&&sum>0){
+		printf("找个厂上班吧");
+	   
+	}
+	else if (sum<750&&sum>710){
+		printf("666,你来考高考太屈才了");
+	    
+	}
+	else
+    {
+		printf("奖杯的，黑客来的，我可得大调查一下你了，答得对有奖励，答不对有惩罚\n");
+		printf ("按任意键开始答题\n"); 
+		_getch(); 
+		char answer;
+    
+  
+        printf("===== 选择题 =====\n");
+        printf("你玩原神吗？\n");
+        printf("1. 你冲Q币吗\n");
+        printf("2. 我是山里灵活的狗\n");
+        printf("3. 酷狗屋檐了\n");
+        printf("4. 吓我一跳我释放忍术\n");
+        printf("请输入答案(1/2/3/4)：");
+        scanf("%s", &answer);
+    
+       
+	
+        switch (answer)
+	 {
+            case '1':{
+			                    for(int i =1 ; i<=100;i++ )
+                            {
+	                            printf("冲！！！");
+                            } 
+					break;
+					 }
+                
+            case '2':{
+	                            for(int i =1 ; i<=100;i++ )
+                           {
+	                            printf("汪汪汪！！！"); 
+                           }
+                break;
+			         }
+            case '3':{
+				
+	                            for(int i =1 ; i<=100;i++ )
+                           {
+	                             printf("区区酷狗，虽远必诛（怒）！！！");
+                           }
+					break;
+                     }
+            case '4':{
+	                           for(int i =1 ; i<=100;i++ )
+                           {
+	                           printf("握握手！！！");   
+                           } 
+					break;
+			         }
+              
+            default:
+			        {
+               
+	                            for(int i =1 ; i<=100;i++ )
+                           {
+	                            printf("原神，启动！！！");
+							
+                           }
+		             break;
+         	        }
+	}  
+} }
